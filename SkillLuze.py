5f327786eb66b8b3640e8c411a16d4124c9071b5
Parad1st <91069729+Parad1st@users.noreply.github.com>
2024-05-20 11:12:35 +0300

    SkillLuze

--- SkillLuze.py
+++ SkillLuze.py
@@ -26,7 +26,7 @@ def setup():
     if not os.path.exists(path):
         os.makedirs(path)
 
-    print("[SETUP] Начинаем настройку...")
+    print("[SETUP] Let's start setting up...")
     def prompt(str):
         valid_input = False
         while not valid_input:
@@ -34,19 +34,19 @@ def setup():
                 number = float(input(str))
                 valid_input = True
             except ValueError:
-                print("[ERROR] Ошибка! Введите только числа (например: 5.2)")
+                print("[ERROR] Error! Enter numbers only (for example: 5.2)")
         return number
 
-    xy_sens = prompt("Чувствительность по X и Y из настроек игры: ")
-    targeting_sens = prompt("Чувствительность при прицеливании из настроек игры: ")
+    xy_sens = prompt("X and Y sensitivity from game settings: ")
+    targeting_sens = prompt("Sensitivity when aiming from game settings: ")
 
     print("[SETUP] SiillLuze")
     sensitivity_settings = {"xy_sens": xy_sens, "targeting_sens": targeting_sens, "xy_scale": 10/xy_sens, "targeting_scale": 1000/(targeting_sens * xy_sens)}
 
     with open('lib/config/config.json', 'w') as outfile:
         json.dump(sensitivity_settings, outfile)
-    print("[!] Сенса настроена")
-    print("[!] Если вы хотите настроить сенсу заного, удалите конфиг файл по пути lib/config/config.json")
+    print("[!] Sensitivity is set")
+    print("[!] If you want to configure the sensor differently, delete the config file along the path lib/config/config.json")
 
 if __name__ == "__main__":
     os.system('cls' if os.name == 'nt' else 'clear')
@@ -61,13 +61,13 @@ if __name__ == "__main__":
     ██████╔╝██║░╚██╗██║███████╗███████╗███████╗╚██████╔╝███████╗███████╗
     ╚═════╝░╚═╝░░╚═╝╚═╝╚══════╝╚══════╝╚══════╝░╚═════╝░╚══════╝╚══════╝
 
-    (Сделал Parad1st)''', "yellow"))
-    print(colored('''Внимание! Данная нейросеть бесплатная и распространяется на GitHub: https://github.com/Parad1st/SkillLuze''', "red"))
+    (By Parad1st)''', "yellow"))
+    print(colored('''Attention! This neural network is free and distributed on GitHub: https://github.com/Parad1st/SkillLuze''', "red"))
 
     path_exists = os.path.exists("lib/config/config.json")
     if not path_exists or ("setup" in sys.argv):
         if not path_exists:
-            print("[ERROR] Сенса не настроена!")
+            print("[ERROR] sensitivity is not set")
         setup()
     path_exists = os.path.exists("lib/data")
     if "collect_data" in sys.argv and not path_exists:
